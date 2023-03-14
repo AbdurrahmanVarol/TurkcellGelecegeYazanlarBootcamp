5adafba84b8abba63afab112fe91778cdfd92104
abdurrahman varol <av_0233@outlook.com>
2023-03-14 12:31:36 +0300

    Homework2- Girilen şifrenin içeriğine göre güçlü, ortda ya da zayıf olduğunu kontrol eden program kodlandı.

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Lütfen şifrenizi giriniz: ");
+var password = Console.ReadLine();
+var result = CheckPassword(password);
+Console.WriteLine(result);
+Console.ReadKey();
+
+string CheckPassword(string password)
+{
+    if (string.IsNullOrWhiteSpace(password))
+        return "Şifre girmediniz.";
+    if (password.Length < 6)
+        return "Şifreniz 6 karakterden az olamaz";
+
+    var hasNumber = password.Any(p => char.IsDigit(p));
+    var hasLetter = password.Any(p => char.IsLetter(p));
+    var hasPunctuatio = password.Any(p => char.IsPunctuation(p));
+
+    if (hasLetter && hasNumber && hasPunctuatio)
+        return "Şifreniz güçlü.";
+    else if ((hasLetter && hasNumber) || (hasLetter && hasPunctuatio) || (hasNumber && hasPunctuatio))
+        return "Şifreniz orta";
+    else
+        return "Şifreniz zayıf";
+}
