e8c4321c05b9b9e9bfb85d0ee0d633a6744105b7
abdurrahman varol <av_0233@outlook.com>
2023-03-24 21:38:39 +0300

    Single Responsibility Principle ve Open Close Principle ile ilgili örnekler

---
+++ Homework5/Homework5/OpenClosePrinciple.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public class OpenClosePrinciple
+    {
+        public OpenClosePrinciple()
+        {
+            //OpenClosePrinciple bir kodun değişime kapalı gelişime açık olması gerektiğini vurgular
+            //Örneğin projede entityframework kullanılırken hibernate e geçilmek istendiğinde proje içerisinde bir aşırı bir değişiklik yapmadan sistemi hibernate geçirmemiz gerekir.
+
+            IProductDal entityframewotkProductDal = new EntityFrameworkProductDal();
+            IProductDal hibernateProductDal = new HibernateProductDal();
+
+            //var productService = new ProductService(entityframewotkProductDal);
+            var productService = new ProductService(hibernateProductDal);
+
+        }
+    }
+    public class ProductService
+    {
+        private readonly IProductDal _productDal;
+        public ProductService(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+    }
+
+    public interface IProductDal
+    {
+        void AddProduct();
+    }
+
+    public class EntityFrameworkProductDal : IProductDal
+    {
+        public void AddProduct()
+        {
+            Console.WriteLine("Product Added with Entityframework");
+        }
+    }
+
+    public class HibernateProductDal : IProductDal
+    {
+        public void AddProduct()
+        {
+            Console.WriteLine("Product Added with Hibernate");
+        }
+    }
+
+}
---
+++ Homework5/Homework5/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,55 @@
+﻿namespace Homework5
+{
+    public class SingleResponsibilityPrinciple
+    {
+        public SingleResponsibilityPrinciple()
+        {
+            //SingleResponsibilityRrinciple bir metodun, class ın tek bir sorumluluğu olması gerektiğini vurgulamaktadır.
+            //ProductRepository class ının görevi ürünleri eklemese mesaj gönderme işlemi ProductRepository içerisinde yapılmamalıdır
+            var messageService = new SmsService();
+
+            var productRepository = new ProductRepository(messageService);
+
+            productRepository.AddProduct();
+
+        }
+    }
+
+    public class ProductRepository
+    {
+        private readonly IMessageService _messageService;
+
+        public ProductRepository(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public void AddProduct()
+        {
+            Console.WriteLine("Product Added!");
+            _messageService.SendMessage("New Product Added!");
+
+        }
+    }
+
+    public interface IMessageService
+    {
+        void SendMessage(string message);
+    }
+
+    public class SmsService : IMessageService
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class EmailService : IMessageService
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
